0dc54488502bc4df4cb939ef63deae93ab3b1946
SequenceDondo <sequelacompany@gmail.com>
2023-02-20 13:15:36 -0800

    java commit

---
+++ MadLibs-StoryGame-Java/storygame.java
@@ -0,0 +1,36 @@
+c class MadLibs {
+	  /*
+	   *   Mad Libs story game, a short story with blank spaces that a player can fill in. 
+	   *     Author: SequenceD
+	   *       Date: 20/02/2023
+	   *         */
+	  	public static void main(String[] args){
+
+			       String name1 = "";
+			             String adjective1 = "";
+				           String adjective2 = "busy";
+					         String adjective3 = "obviously"; 
+						       String verb1 = "dance";
+						             String noun1 = "teenagers";
+							           String noun2 = "junk food";
+								         String noun3 = "raggae music";
+									       String noun4 = "old passerbys";
+									             String noun5 = "fire";
+										           String noun6 = "ice";
+											         String name2 = "Danny";
+												       int number = 5000;
+												             String place1 = "netherlands";
+
+
+
+
+
+
+													           
+													           //The template for the story
+														   //      String story = "This morning "+name1+" woke up feeling "+adjective1+". 'It is going to be a "+adjective2+" day!' Outside, a bunch of "+noun1+"s were protesting to keep "+noun2+" in stores. They began to "+verb1+" to the rhythm of the "+noun3+", which made all the "+noun4+"s very "+adjective3+". Concerned, "+name1+" texted "+name2+", who flew "+name1+" to "+place1+" and dropped "+name1+" in a puddle of frozen "+noun5+". "+name1+" woke up in the year "+number+", in a world where "+noun6+"s ruled the world.";
+														   //
+														   //            System.out.println(story);
+														   //                }       
+														   //                }
+														   //
